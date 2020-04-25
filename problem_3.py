58c6a16931185d3fc6356b01eb84975cdc52077b
Anton <akrevetka@gmail.com>
2020-04-25 14:34:15 +0700

    day 2

---
+++ problem_3.py
@@ -0,0 +1,33 @@
+from math import sqrt
+
+
+def decor(func):
+    def inner_dec(n):
+        print('test decorator')
+        print('sqrt n: ' + str(sqrt(n) + 1))
+        print('int sqrt n: ' + str(int(sqrt(n) + 1)))
+        return func(n)
+    return inner_dec
+
+
+@decor
+def get_simple_dev_list(n):
+    simple_dev_list = []
+    dev_list = [dev_i for dev_i in range(1, int(sqrt(n) + 1), 2) if n % dev_i == 0]    # [dev_i for dev_i in range(1, n // 2 + 1, 2) if n % dev_i == 0]
+    dev_list.append(n)
+    for i in dev_list:
+        count = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                count += 1
+            if i != j and count == 2:
+                break
+            elif i == j and count == 2:
+                simple_dev_list.append(i)
+    return simple_dev_list
+
+
+# simple_dev_list = get_simple_dev_list(13195)
+simple_dev_list = get_simple_dev_list(600851475143)
+print(simple_dev_list)
+print(simple_dev_list[-1])
